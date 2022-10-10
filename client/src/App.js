e485910283d31debfd6ff825aa86e15553bc599b
Zaidbhati10114 <zaidbhati007@gmail.com>
2022-10-10 16:22:34 +0530

    Frontend Started|| Added header

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>Hello App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
